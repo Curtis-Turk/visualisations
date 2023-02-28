a4983f5b876accebc4e7b86780417185b7467ebb
Curtis Turk <curtis.c.turk@outlook.com>
2023-02-28 18:03:01 +0000

    Testing a line

--- flowfield/script.js
+++ flowfield/script.js
@@ -3,4 +3,26 @@ window.onload = function () {
   const ctx = canvas.getContext("2d");
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+  const flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
 };
+
+class FlowFieldEffect {
+  #ctx;
+  #width;
+  #height;
+
+  constructor(ctx, width, height) {
+    this.#ctx = ctx;
+    this.#ctx.strokeStyle = "white";
+    this.#width = width;
+    this.#height = height;
+    console.log("flow field created");
+    this.#draw(100, 100);
+  }
+  #draw(x, y) {
+    this.#ctx.beginPath();
+    this.#ctx.moveTo(x, y);
+    this.#ctx.lineTo(x + 100, y + 100);
+    this.#ctx.stroke();
+  }
+}
