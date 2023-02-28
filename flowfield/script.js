893b13eb6c2fea389c46044ac5a3be20c0ace735
Curtis Turk <curtis.c.turk@outlook.com>
2023-02-28 18:11:09 +0000

    added an animation to the line

--- flowfield/script.js
+++ flowfield/script.js
@@ -4,6 +4,7 @@ window.onload = function () {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   const flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
+  flowField.animate();
 };
 
 class FlowFieldEffect {
@@ -17,12 +18,24 @@ class FlowFieldEffect {
     this.#width = width;
     this.#height = height;
     console.log("flow field created");
-    this.#draw(100, 100);
+
+    this.x = 0;
+    this.y = 0;
   }
+
   #draw(x, y) {
+    const length = 300;
     this.#ctx.beginPath();
     this.#ctx.moveTo(x, y);
-    this.#ctx.lineTo(x + 100, y + 100);
+    this.#ctx.lineTo(x + length, y + length);
     this.#ctx.stroke();
+  }
+
+  animate() {
+    console.log("animating");
+    this.#draw(this.x, this.y);
+    this.x += 1;
+    this.y += 0.5;
+    requestAnimationFrame(this.animate.bind(this));
   }
 }
